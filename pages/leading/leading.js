6c8c2a06b123c2044c9fd1bcf994cf902b5c4378
josymarss <sylvajosymar@gmail.com>
2021-07-28 02:20:07 +0100

    Homepage almost done!

--- pages/leading/leading.js
+++ pages/leading/leading.js
@@ -2,7 +2,10 @@ import {useEffect, useState} from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { Github,faCode } from "@fortawesome/free-solid-svg-icons"; // import the icons you need
+import{
+    Github,faCode,faCreditCard,faLessThanEqual,faBook,
+    faGlobeAfrica, faVideo,faInstagram,faCodeFork,faDiscord } 
+from "@fortawesome/free-solid-svg-icons"; // import the icons you need
 
 import leading from './leading.module.css'
 
@@ -42,36 +45,36 @@ export default function Leading(){
             <section className={leading.musthave}>
                 <h2>{`Compromisso com todos os níveis de Ngola.`}</h2>
                 <p>{`Ensinamentos nas mais diversas áreas, as bases, aqui são prioridades.`}</p>
-                <h2><span>{`Obrigatorio`}</span></h2>
+                <h2><span>{`Obrigatório`}</span></h2>
                 <div className={leading.courseContainer}>
                     <div className={leading.courses}>
-                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={leading.courseIcon}icon={faLessThanEqual}></FontAwesomeIcon>
                         <h3>{`Bases matemática`}</h3>
                         <p>{`Todas as bases funcdamentais para compreender o mundo.`}</p>
                     </div>
                     <div className={leading.courses}>
-                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={leading.courseIcon}icon={faCode}></FontAwesomeIcon>
                         <h3>{`Programação para todos`}</h3>
                         <p>{`Aprenda a pensar. dominar as novas tecnologias, cria um aplicativo, site ou um jogo para seu celular`}</p>
                     </div>
                     <div className={leading.courses}>
-                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={leading.courseIcon}icon={faGlobeAfrica}></FontAwesomeIcon>
                         <h3>{`Inglês`}</h3>
                         <p>{`Domine o idioma internacional, e tenha grandes oportunidades, como as do mundo a fora.`}</p>
                     </div>
                     <div className={leading.courses}>
-                        <FontAwesomeIcon icon={faCode}></FontAwesomeIcon>
+                        <FontAwesomeIcon className={leading.courseIcon}icon={faBook}></FontAwesomeIcon>
                         <h3>{`Kimbundu`}</h3>
                         <p>{`Um dia essa língua se tornara oficial, e lembre-se, um povo sem cultura é um povo, sem origem`}</p>
                     </div>
                 </div>
             </section>
 
-            <section className={leading.partners}>
+            {/* <section className={leading.partners}>
                 <h2>Parceiros</h2>
                 <p>Algumas pessoas que confiam no noso trabalho</p>
-                {/*Som eimages here of th partners*/}
-            </section>
+                //Som eimages here of th partners
+            </section> */}
 
             <section className={leading.payment}>
                 <h2>{`O ensino sempre será gratuíto`}</h2>
@@ -82,7 +85,7 @@ export default function Leading(){
                         na secção apoirar e dentre eles com seu cartão, de eleição`
                     }
                 </p>
-                <Image alt='payment' width={40} height={40} src='/favicon'/>
+                <FontAwesomeIcon className={leading.fontawsomeicon}size={80} icon={faCreditCard}></FontAwesomeIcon>
             </section>
 
             <section className={leading.events}>
@@ -94,29 +97,30 @@ export default function Leading(){
                             as tuas habilidades de programação
                             e ganhares, um trofeu.`}
                         </p>
-                        <p>{`10 á 15 ago`}</p>
+                        <p><span>{`10 á 15 ago`}</span></p>
                     </div>
-                    <Image alt='Event mona ya kode' width={40} height={40} src='/favicon' />
+                    <Image alt='Event mona ya kode' width={487.24} height={380.47} src='/unboxing.png' />
                 </div>
             </section>
 
             <section className={leading.media}>
                 <div className={leading.youtube}>
-                    <Image alt='Event mona ya kode' width={40} height={40} src='/favicon' />
+                <FontAwesomeIcon className={leading.youtubeIcon}icon={faVideo}></FontAwesomeIcon>
                     <h2>Youtube</h2>
-                    <p>{`Nós temos um canal 
+                    <p><span>{`Nós temos um canal 
                         no youtube onde você
                         pode acompanhar as 
-                        aulas.`}
+                        aulas.`}</span>
                     </p>
                 </div>
                 <div className={leading.others}>
-                    <Image alt='Instagram' width={40} height={40} src='/favicon'/>
-                    <Image alt='Discord' width={40} height={40} src='/favicon' />
-                    <Image alt='Github' width={40} height={40} src='/favicon' />
+                    
+                    <FontAwesomeIcon className={leading.mediaSocial} icon={faCodeFork}></FontAwesomeIcon>
+                    
                 </div>
-                <p>{`@2021 bantu_dev`}</p>
+                
             </section>
+            <footer>{`@2021 bantu_dev`}</footer>
         </div>
     )
 }
